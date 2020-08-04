154c710701aa6e0aa587fd27a213e3d0be0d2b78
Jordan Thomas <jordanthomas761@gmail.com>
2020-08-04 02:00:58 -0400

    verify that a single book can be retrieved

--- src/test/java/site/jordanthomas/BookStore/BookStoreApplicationTests.java
+++ src/test/java/site/jordanthomas/BookStore/BookStoreApplicationTests.java
@@ -6,15 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql("/BooksSqlTest.sql")
+@Transactional
 class BookStoreApplicationTests {
 
 	@Autowired
@@ -22,9 +27,19 @@ class BookStoreApplicationTests {
 
 	@Test
 	void getAllBooks() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/books")
-		.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
+		mockMvc.perform(MockMvcRequestBuilders
+				.get("/books")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
 	}
 
+	@Test
+	void getSingleBook() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+				.get("/books/1")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
 }
