73335d97771416587ecca4d5be048e0936ca0b72
Jordan Thomas <jordanthomas761@gmail.com>
2020-08-04 02:07:24 -0400

    Add Sql resource

---
+++ src/test/resources/BooksSqlTest.sql
@@ -0,0 +1,4 @@
+INSERT INTO book (id, title, author, price)
+VALUES (1, 'Shadow and Shrek', 'Fangirl', 6.23);
+INSERT INTO book (id, title, author, price)
+VALUES (2, 'Shadow and Shrek', 'Fangirl', 6.23);
